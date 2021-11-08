537f9b5c2c5b3cbb057d70239981c2d408190413
Antonella García <alu0101227610@ull.edu.es>
2021-11-08 11:51:12 +0000

    parse

--- gh-repo-rename-alu0101227610.js
+++ gh-repo-rename-alu0101227610.js
@@ -1,3 +1,4 @@
+#! /Users/fuego/.nvm/versions/node/v17.0.1/bin/node
 const ins = require("util").inspect;
 const deb = (...args) => { 
     if (debug) console.log(ins(...args, {depth: null})); 
@@ -6,3 +7,16 @@ const deb = (...args) => {
 const fs = require("fs");
 const shell = require('shelljs');
 const { Command } = require('commander');
+const program = new Command();
+
+program
+  .option('-r, --repo <repo>', 'repository')
+  .option('-o, --org <org>', 'org')
+  .option('-h, --help <help>', 'help');
+
+program.parse(process.argv);
+
+  const options = program.opts();
+  if (options.repo) console.log(`repository: ${options.repo}`);
+  if (options.org) console.log(`org: ${options.org}`);
+  if (options.help) console.log(`help: ${options.help}`);
