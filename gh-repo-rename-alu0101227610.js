f883f1da85086c07b6afb39e52a8da3a9a4d71d4
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-11-10 12:34:03 +0000

    parsing de los argumentos funciona

--- gh-repo-rename-alu0101227610.js
+++ gh-repo-rename-alu0101227610.js
@@ -1,8 +1,7 @@
-#! /Users/fuego/.nvm/versions/node/v17.0.1/bin/node
-
+#! /usr/bin/env node
 const ins = require("util").inspect;
-const deb = (...args) => { 
-    if (debug) console.log(ins(...args, {depth: null})); 
+const deb = (...args) => {
+  if (debug) console.log(ins(...args, { depth: null }));
 };
 
 const fs = require("fs");
@@ -10,7 +9,6 @@ const shell = require('shelljs');
 const { Command } = require('commander');
 const program = new Command();
 const { version } = require("./package.json")
-const { args } = program;
 
 program
   .version(version)
@@ -19,14 +17,26 @@ program
 
 program.parse(process.argv);
 
-const {org, repo} = program.opts();
+let args = program.args;
 
-const options = program.opts();
-  if (repo) console.log(`repository: ${repo}`);
-  if (org) console.log(`org: ${org}`);
+let { org, repo } = program.opts();
+console.log(`org y repo ${org} ${repo}`)
+
+
+if (repo) console.log(`repository: ${repo}`);
+if (org) console.log(`org: ${org}`);
 
 if (!shell.which('git')) shell.echo("git not installed")
 if (!shell.which('gh')) shell.echo("gh not installed");
 
-if (!(org && repo && args.length === 1)) program.help();    
-else if (args.length < 2) program.help();
+let newName;
+if (!org) {
+  [org, repo] = args[0].split("/");
+  console.log(`org y repo ${org} ${repo}`)
+
+  newName = args[1]
+  console.log(`newName = ${newName}`)
+}
+if (!newName) newName = args[0]
+
+if (!org || !repo || !newName) program.help()
