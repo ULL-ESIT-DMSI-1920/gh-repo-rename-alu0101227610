e29fb9f7cc4a85309db5aacc3beed2eb283459a5
Antonella García <alu0101227610@ull.edu.es>
2021-11-10 12:06:25 +0000

    no funciona js

--- gh-repo-rename-alu0101227610.js
+++ gh-repo-rename-alu0101227610.js
@@ -1,4 +1,5 @@
 #! /Users/fuego/.nvm/versions/node/v17.0.1/bin/node
+
 const ins = require("util").inspect;
 const deb = (...args) => { 
     if (debug) console.log(ins(...args, {depth: null})); 
@@ -8,27 +9,24 @@ const fs = require("fs");
 const shell = require('shelljs');
 const { Command } = require('commander');
 const program = new Command();
-
-program.version('8.3.0')
+const { version } = require("./package.json")
+const { args } = program;
 
 program
+  .version(version)
   .option('-r, --repo <repo>', 'repository')
   .option('-o, --org <org>', 'org');
 
 program.parse(process.argv);
 
-  const options = program.opts();
-  if (options.repo) console.log(`repository: ${options.repo}`);
-  if (options.org) console.log(`org: ${options.org}`);
+const {org, repo} = program.opts();
 
-if (!shell.which(git)) shell.echo("git not installed")
-if (!shell.which(gh)) shell.echo("gh not installed");
+const options = program.opts();
+  if (repo) console.log(`repository: ${repo}`);
+  if (org) console.log(`org: ${org}`);
 
+if (!shell.which('git')) shell.echo("git not installed")
+if (!shell.which('gh')) shell.echo("gh not installed");
 
-const org = options.org;
-if (!org) {
-    if(program.args.length < 2) {
-        console.log("No arguments specified. sending help")
-        program.help();
-    }
-}
+if (!(org && repo && args.length === 1)) program.help();    
+else if (args.length < 2) program.help();
