a86fb60024cfb725a56f70a4c7ae60113a922f73
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2021-11-17 10:50:38 +0000

    funciona

--- gh-repo-rename-alu0101227610.js
+++ gh-repo-rename-alu0101227610.js
@@ -1,10 +1,6 @@
 #! /usr/bin/env node
 const ins = require("util").inspect;
-const deb = (...args) => {
-  if (debug) console.log(ins(...args, { depth: null }));
-};
 
-const fs = require("fs");
 const shell = require('shelljs');
 const { Command } = require('commander');
 const program = new Command();
@@ -22,24 +18,10 @@ let args = program.args;
 
 let { org, repo, name } = program.opts();
 
-if (repo) console.log(`repository: ${repo}`);
-if (org) console.log(`owner: ${org}`);
+if (!org || ! repo || !name) program.help();
 
 if (!shell.which('git')) shell.echo("git not installed")
 if (!shell.which('gh')) shell.echo("gh not installed");
 
-if(program.args.length < 1) program.help();
-
-let newName;
-
-if (!newName) newName = args[0]
-if (!org || !repo || !newName) program.help()
-
-if (!org) {
-  [org, repo] = args[0].split("/");
-  console.log(`owner: ${org} repository: ${repo}`)
-
-  newName = args[1]
-  shell.exec(`gh api -X PATCH /repos/${org}/${repo} -f name=${name} --jq .[].name`);
-}
+shell.exec(`gh api -X PATCH /repos/${org}/${repo} -f name=${name}`);
 
