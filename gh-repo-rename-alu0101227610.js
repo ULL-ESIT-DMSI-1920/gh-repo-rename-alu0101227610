8fbf7a99ceece5c03292f21b93265404cf9ad563
Antonella García <alu0101227610@ull.edu.es>
2021-11-29 15:31:53 +0000

    minor update

--- gh-repo-rename-alu0101227610.js
+++ gh-repo-rename-alu0101227610.js
@@ -21,15 +21,13 @@ let originalName = `${program.opts().name}`;
 let { org, repo, name } = program.opts();
 // console.log(originalName);
 
-if (!org || ! repo || !name) program.help();
-
-if (!org || !repo || !name) program.help();
-
 if (!shell.which('git')) shell.echo("git not installed")
 if (!shell.which('gh')) shell.echo("gh not installed");
+
+if (!org || ! repo || !name) program.help();
+if (!org || !repo || !name) program.help();
 
 let r = shell.exec(`gh api -X PATCH /repos/${org}/${repo} -f name=${name}`, {silent: true});
 
 let rj = JSON.parse(r.stdout)
-//console.log(`The repo ${org}/${originalName} has been renamed to ${rj.full_name}`);
 console.log(`The repo has been renamed to ${rj.full_name}`);
